421e1ebbff3281c09e0f874fecf7ff954d997095
alexndermuriithi <alexander.muriithi@outlook.com>
2016-06-23 10:53:18 +0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class = "row footer">
+	<div class = "col-md-4 col-md-offset-4">
+		<center>
+			<h4>Follow us</h4>
+			<div class = "col-md-3"><a href="http://www.facebook.com/mywedding"><img width = 50px height = 50px src = "http://www.algonquincollege.com/infosec/files/2016/01/35740AA3584B4E43AB84254C17B18982.png"></a></div>
+			<div class = "col-md-3"><a href="#"><img width = 50px height = 50px src = "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/twitter_circle_color-256.png"></a></div>
+			<div class = "col-md-3"><a href="http://www.instagram.com/mywedding"><img width = 50px height = 50px src = "https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-128.png"></a></div>
+			<div class = "col-md-3"><a href="http://www.pintrest.com/mywedding"><img width = 50px height = 50px src = "http://icons.iconarchive.com/icons/martz90/circle/512/pinterest-icon.png"></a></div>
+			<p> Copyright myWedding<span class="glyphicon glyphicon-copyright-mark">. </p>
+		</center>
+	</div>
+</div>
---
+++ header.php
@@ -0,0 +1,34 @@
+<div class = "jumbotron">
+	<h1>myWedding</h1>
+	<small>Your Planning Partner</small>
+</div>
+		
+<nav class = "navbar navbar-default navbar-fixed-top">
+	<div class = "container-fluid">
+		<div class="navbar-header">
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="HomePage.html">myWedding</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="HomePage.html" class="btn btn-link" role="button"><span class="glyphicon glyphicon-home"> Home</a></li>
+				<li><a href="Gallery.html" class="btn btn-link" role="button">Gallery</a></li>
+				<li class="dropdown"><a class="dropdown-toggle btn btn-link" data-toggle="dropdown" href="AboutUs.html">About<span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li class = "disabled"><a href="AboutUs.html" class="btn btn-link" role="button">About Us</a></li>
+						<li class = "disabled"><a href="Developers.html" class="btn btn-link" role="button">Developers</a></li>
+					</ul>
+				</li>
+				<li class = "disabled"><a href="" class="btn btn-link" role="button">add page</a></li>						
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="SignUp.html" class="btn btn-link" role="button"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+				<li><a href="#Login" class="btn btn-link" role="button"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
